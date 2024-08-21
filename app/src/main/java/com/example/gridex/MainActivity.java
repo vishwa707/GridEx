305a71934447065cb67c6d32370c483f607642ed
Vishwa Tej <ixsaac707@gmail.com>
2024-08-21 09:56:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gridex/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.gridex;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    GridView gv;
+    String[] n = new String[15];
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        for(int i=0; i<n.length; i++){
+            n[i]="Grid"+i;
+        }
+        gv=(GridView)findViewById(R.id.grid);
+        ArrayAdapter<String> a = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, n);
+        gv.setAdapter(a);
+
+        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "Clicked:"+i, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
